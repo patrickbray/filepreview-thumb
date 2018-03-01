a5fd3a21580cb87cfb0138674e9792aab729aa46
Patrick Bray <pbray@atlassian.com>
2018-03-02 11:06:47 +1300

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var filepreview = require('filepreview');
 
-const inputUrl = 'https://s3.amazonaws.com/raken-dev/2018/02/31566d4d-fd5d-4af4-9194-856492d2baea';
 const inputFile = '/opt/thumb-generator/sample.pdf';
 const outputFile = './myPdf_preview.gif';
 
